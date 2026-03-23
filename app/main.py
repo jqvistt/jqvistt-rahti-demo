f61e0a4bed845c71a2c0456bc805e96cfa9ea388
Jqvistt <joakim.is.lonnqvist@gmail.com>
2026-03-23 11:50:03 +0200

    Change Message

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hello!", "v": "0.1" }
+    return { "msg": "Hey! I changed the message you silly goober.", "v": "0.1" }
 
 
 @app.get("/items/{id}")
