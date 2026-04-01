61b904182b48361c14b0edc702e7b5433def6827
Jqvistt <joakim.is.lonnqvist@gmail.com>
2026-04-01 10:53:53 +0300

    Add rooms endpoint to return room details

--- app/main.py
+++ app/main.py
@@ -17,6 +17,20 @@ async def read_root(request: Request):
     client_ip = request.client.host
     return {"client_ip": client_ip}
 
+# En liten placeholder array av objekt som representerar rum. Byts ut mot databas
+rooms = [
+    {"room_id": 1, "room_number": "101", "type": "Enkelrum", "price": 850, "occupied": False},
+    {"room_id": 2, "room_number": "102", "type": "Enkelrum", "price": 850, "occupied": True},
+    {"room_id": 3, "room_number": "201", "type": "Dubbelrum", "price": 1250, "occupied": False},
+    {"room_id": 4, "room_number": "202", "type": "Dubbelrum", "price": 1250, "occupied": False},
+    {"room_id": 5, "room_number": "301", "type": "Svit", "price": 2900, "occupied": False},
+    {"room_id": 6, "room_number": "302", "type": "Svit", "price": 2900, "occupied": True}
+]
+
+@app.get("/api/rooms")
+def read_rooms():
+    return {"rooms": rooms}
+
 @app.get("/")
 def read_root():
     return { "msg": "Hey! I changed the message you silly goober.", "v": "0.1" }
