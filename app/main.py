acf3392cf3a639fae96763ffcd3933e4dcdcffb0
Jqvistt <joakim.is.lonnqvist@gmail.com>
2026-04-01 09:53:09 +0300

    Fix CORS Error

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware  
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allor_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/ip")
 async def read_root(request: Request):
